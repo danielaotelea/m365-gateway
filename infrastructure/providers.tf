7b4b675d3efbcbaebd4a4bac886ee1111189a764
daniela.otelea <daniela.otelea@gmail.com>
2024-07-10 14:04:00 +0200

    update the infra 3

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,7 +1,9 @@
-
-
 # Backend configuration - store the state file in Azure Storage
-#terraform {
-#  backend "azurerm" {
-#  }
-#}
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "m365-gateway-rg"
+    storage_account_name = "m365storageaccount"
+    container_name       = "m365-gateway-container"
+    key                  = "terraform.tfstate"
+  }
+}
