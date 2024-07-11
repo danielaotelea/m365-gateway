2ed2d97df8c831e1719a77396f051e53e05bd56f
daniela.otelea <daniela.otelea@gmail.com>
2024-07-11 11:23:10 +0200

    Add new Function container for PS

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -41,7 +41,26 @@ resource "azurerm_application_insights" "example" {
 
 
 resource "azurerm_linux_function_app" "example" {
-  name                        = local.function_app_name
+  name                        = local.function_java_app_name
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  storage_account_name        = azurerm_storage_account.example.name
+  storage_account_access_key  = azurerm_storage_account.example.primary_access_key
+  service_plan_id             = azurerm_service_plan.example.id
+  functions_extension_version = local.functions_extension_version
+  site_config {
+    application_stack {
+      java_version = local.java_version
+    }
+  }
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.example.instrumentation_key
+  }
+  depends_on = [azurerm_storage_account.example]
+}
+
+resource "azurerm_linux_function_app" "example2" {
+  name                        = local.function_ps_app_name
   location                    = azurerm_resource_group.example.location
   resource_group_name         = azurerm_resource_group.example.name
   storage_account_name        = azurerm_storage_account.example.name
