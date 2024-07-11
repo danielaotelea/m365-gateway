6bdaa0331c2787d24e072a02b48167632212a683
daniela.otelea <daniela.otelea@gmail.com>
2024-07-11 11:00:32 +0200

    Funct deployment at PR closed

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -58,3 +58,10 @@ resource "azurerm_linux_function_app" "example" {
   }
   depends_on = [azurerm_storage_account.example]
 }
+
+resource "azurerm_storage_container" "example" {
+  name                  = local.container_name
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+  depends_on            = [azurerm_storage_account.example]
+}
